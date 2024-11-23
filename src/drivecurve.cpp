a41e4eb8fb28e153de23dd1be044badac0fea8e2
Kaushal Dabbiru <92063273+TheMaster3558@users.noreply.github.com>
2024-11-22 21:14:28 -0800

    Add comments

--- src/drivecurve.cpp
+++ src/drivecurve.cpp
@@ -1,14 +1,17 @@
 #include "drivecurve.hpp"
 
 
+// Call the parent classes constructor and also assign the maxband variable
 DoubleBandedExpoDriveCurve::DoubleBandedExpoDriveCurve(
     float deadband, float maxband, float minOutput, float curve
 ) : lemlib::ExpoDriveCurve(deadband, minOutput, curve), maxband(maxband) {}
 
 
 float DoubleBandedExpoDriveCurve::curve(float input) {
+    // if the input is over the maxband value return the max power
     if (input > maxband) {
         return 127;
     }
+    // otherwise let the parent class's method do its algorithim
     return lemlib::ExpoDriveCurve::curve(input);
 }

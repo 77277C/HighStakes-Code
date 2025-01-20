1460a52d68e76bd69173a7e19e8b107b4c161e3f
Arnav Mittal <Arnavmittal50@gmail.com>
2025-01-20 12:59:43 -0800

    more voltage

--- src/autons.cpp
+++ src/autons.cpp
@@ -9,7 +9,7 @@ void red_left_after_mogo() {
       .earlyExitRange = 4
    });
 
-   chassis.moveToPose(-13, 52, 0, 5000, {
+   chassis.moveToPose(-16, 50, 0, 5000, {
       .lead = 0.6,
       .maxSpeed = 60,   
       .earlyExitRange = 1
@@ -27,7 +27,7 @@ void red_left_after_mogo() {
 
    pros::delay(500);
 
-    chassis.moveToPoint(-24, 48, 3000, {
+    chassis.moveToPoint(-30, 49, 3000, {
        .maxSpeed = 100,
        .earlyExitRange = 0.5
    });
@@ -57,7 +57,10 @@ void red_left_after_mogo() {
    chassis.waitUntilDone();
 
    chassis.moveToPoint(-29, 0, 800);
+   intake.move_percentage(100);
    chassis.waitUntilDone();
+   intake.move_percentage(100);
+   pros::delay(2000);
 }
 
 void red_left() {
@@ -98,7 +101,7 @@ void solo_awp_red_left() {
    });
    chassis.waitUntil(3);
    chassis.cancelMotion();
-   chassis.moveToPose(-25, 21, 245, 4000, {
+   chassis.moveToPose(-25, 23.5, 245, 4000, {
    
       .forwards = false,
 
@@ -110,7 +113,7 @@ void solo_awp_red_left() {
    ladybrown.set_current_target(LadyBrown::AWAY);
    chassis.waitUntilDone();
     
-   chassis.moveToPoint(-24, 21.5, 1500, {
+   chassis.moveToPoint(-24, 23.5, 1500, {
       .forwards = false,
       .maxSpeed = 90,
       .minSpeed = 60,
@@ -132,7 +135,7 @@ void blue_right_after_mogo() {
       .earlyExitRange = 4
    });
    intake.move_percentage(100);
-   chassis.moveToPose(13, 52, 0, 5000, {
+   chassis.moveToPose(15.5, 52, 0, 5000, {
       .lead = 0.55,
       .maxSpeed = 60,   
       .earlyExitRange = 1
@@ -153,13 +156,13 @@ void blue_right_after_mogo() {
 
    pros::delay(500);
 
-    chassis.moveToPoint(28, 48, 3000, {
+    chassis.moveToPoint(29.5, 48, 3000, {
        .maxSpeed = 100,
        .earlyExitRange = 0.5
    });
    chassis.waitUntilDone();
 
-   chassis.moveToPoint(46, 5, 3500, {
+   chassis.moveToPoint(47.5, 2, 3500, {
       .maxSpeed = 100,
    });
 
@@ -167,7 +170,7 @@ void blue_right_after_mogo() {
    intake_raise.set_state(true);
    chassis.cancelMotion();
    
-   chassis.moveToPoint(46, 5, 3500, {
+   chassis.moveToPoint(47.2, 2, 3500, {
       .maxSpeed = 50,
    });
    chassis.waitUntilDone();
@@ -176,7 +179,7 @@ void blue_right_after_mogo() {
    
    pros::delay(300);
    
-   chassis.moveToPoint(41, 10, 3500, {
+   chassis.moveToPoint(46, 5, 3500, {
       .forwards = false,
       .maxSpeed = 100,
    });
@@ -221,7 +224,7 @@ void blue_right_alliance_stake() {
    });
    chassis.waitUntil(3);
    chassis.cancelMotion();
-   chassis.moveToPose(28, 21, 115, 4000, {
+   chassis.moveToPose(28, 22, 115, 4000, {
    
       .forwards = false,
 
@@ -233,7 +236,7 @@ void blue_right_alliance_stake() {
    ladybrown.set_current_target(LadyBrown::AWAY);
    chassis.waitUntilDone();
     
-   chassis.moveToPoint(26.5, 21.5, 1500, {
+   chassis.moveToPoint(26.5, 22.5, 1500, {
       .forwards = false,
       .maxSpeed = 90,
       .minSpeed = 60,
@@ -471,9 +474,9 @@ void skills() {
    pros::delay(200);
    clamp.set_state(false);
    // side 2
-   chassis.moveToPose(-41, 0, 180, 6000, {.earlyExitRange = 2});
+   chassis.moveToPose(-40, 0, 180, 6000, {.earlyExitRange = 2});
 
-   chassis.moveToPoint(-41, -28, 2500, {.forwards = false, .earlyExitRange = 1});
+   chassis.moveToPoint(-40, -28, 2500, {.forwards = false, .earlyExitRange = 1});
    chassis.waitUntilDone();
    clamp.set_state(true);
    pros::delay(1000);

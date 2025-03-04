72e5f33c0b4b9a13342e57fe8e70e9866f8c2a4d
TheMaster3558 <chawk_k@outlook.com>
2025-03-03 20:44:31 -0800

    Update ports

--- src/devices.cpp
+++ src/devices.cpp
@@ -8,8 +8,8 @@ pros::adi::Pneumatics doinker_piston('C', false);
 pros::MotorGroup left_motors({-13, -12, -9}, pros::MotorGears::blue);
 pros::MotorGroup right_motors({8, 15, 4}, pros::MotorGears::blue);
 
-pros::MotorGroup intake_hooks({-19}, pros::MotorGears::blue);
-pros::MotorGroup intake_front({-18}, pros::MotorGears::blue);
+pros::MotorGroup intake_hooks({19}, pros::MotorGears::blue);
+pros::MotorGroup intake_front({18}, pros::MotorGears::blue);
 
 pros::Imu imu(17);
 pros::Optical intake_optical(10);
